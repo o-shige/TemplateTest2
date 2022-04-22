a7441006e1a043b7caa36f746efcf05d9fc34a96
o-shige <hogehoge@hoge.com>
2022-04-22 16:09:07 +0900

    foramatterで成形

--- app/src/main/java/demo/ScoreBoard.java
+++ app/src/main/java/demo/ScoreBoard.java
@@ -1,150 +1,168 @@
 package demo;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ScoreBoard {
 
-    ArrayList<Integer> balls = new ArrayList<Integer>();
+  ArrayList<Integer> balls = new ArrayList<Integer>();
 
-    private static class Frame {
-        int itsFrame = 1;
+  private static class Frame {
 
-        private void next() {
-            itsFrame++;
-        }
+    int itsFrame = 1;
 
-        private boolean isLast() {
-            return itsFrame >= 10;
-        }
+    private void next() {
+      itsFrame++;
     }
 
-    interface State {
-        int calcScore(List<Integer> balls, int currentScore, int currentIndex);
+    private boolean isLast() {
+      return itsFrame >= 10;
+    }
+  }
 
-        boolean needsNextFrame();
+  interface State {
+    int calcScore(List<Integer> balls, int currentScore, int currentIndex);
 
-        State next();
+    boolean needsNextFrame();
+
+    State next();
+  }
+
+  static class FirstThrow implements State {
+
+    boolean needsNextFrame = false;
+    boolean isFirstScore = true;
+
+    @Override
+    public int calcScore(
+      List<Integer> balls,
+      int currentScore,
+      int currentIndex
+    ) {
+      var result = 0;
+      if (isStrike(currentScore)) {
+        result =
+          calcFirstThrowBallScoreIfStrike(balls, currentScore, currentIndex);
+        needsNextFrame = true;
+      } else {
+        isFirstScore = false;
+      }
+      return result;
     }
 
-    static class FirstThrow implements State {
-
-        boolean needsNextFrame = false;
-        boolean isFirstScore = true;
-
-        @Override
-        public int calcScore(List<Integer> balls, int currentScore, int currentIndex) {
-            var result = 0;
-            if (isStrike(currentScore)) {
-                result = calcFirstThrowBallScoreIfStrike(balls, currentScore, currentIndex);
-                needsNextFrame = true;
-            } else {
-                isFirstScore = false;
-            }
-            return result;
-        }
-
-        @Override
-        public boolean needsNextFrame() {
-            return needsNextFrame;
-        }
-
-        @Override
-        public State next() {
-            if (isFirstScore) {
-                return this;
-            } else {
-                return new SecondThrow();
-            }
-        }
-
-        private boolean isStrike(int score) {
-            if (score == 10) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        private int calcFirstThrowBallScoreIfStrike(List<Integer> balls, int score, int currentIndex) {
-            var res = 0;
-
-            if (currentIndex + 1 < balls.size()) {
-                res += balls.get(currentIndex + 1);
-            }
-            if (currentIndex + 2 < balls.size()) {
-                res += balls.get(currentIndex + 2);
-            }
-            return res;
-        }
-
+    @Override
+    public boolean needsNextFrame() {
+      return needsNextFrame;
     }
 
-    static class SecondThrow implements State {
-
-        @Override
-        public int calcScore(List<Integer> balls, int currentScore, int currentIndex) {
-            var result = 0;
-            if (isSpare(balls, currentScore, currentIndex)) {
-                result = calcSecondThrowBallScoreIfSpare(balls, currentScore, currentIndex);
-            }
-
-            return result;
-        }
-
-        @Override
-        public boolean needsNextFrame() {
-            return true;
-        }
-
-        @Override
-        public State next() {
-            return new FirstThrow();
-        }
-
-        private boolean isSpare(List<Integer> balls, int score, int currentIndex) {
-            if (score + balls.get(currentIndex - 1) == 10) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        private int calcSecondThrowBallScoreIfSpare(List<Integer> balls, int score, int currentIndex) {
-            var res = 0;
-
-            if (currentIndex + 1 < balls.size()) {
-                res += balls.get(currentIndex + 1);
-            }
-
-            return res;
-        }
+    @Override
+    public State next() {
+      if (isFirstScore) {
+        return this;
+      } else {
+        return new SecondThrow();
+      }
     }
 
-    public void throwBall(int score) {
-        balls.add(score);
+    private boolean isStrike(int score) {
+      if (score == 10) {
+        return true;
+      } else {
+        return false;
+      }
     }
 
-    public int score() {
-        var itsScore = 0;
+    private int calcFirstThrowBallScoreIfStrike(
+      List<Integer> balls,
+      int score,
+      int currentIndex
+    ) {
+      var res = 0;
 
-        var frame = new Frame();
-        State state = new FirstThrow();
-
-        for (int i = 0; i < balls.size(); i++) {
-            var currentScore = balls.get(i);
-            itsScore += currentScore;
-
-            if (frame.isLast()) {
-                continue;
-            }
-
-            itsScore += state.calcScore(balls, currentScore, i);
-            if (state.needsNextFrame()) {
-                frame.next();
-            }
-            state = state.next();
-        }
-        return itsScore;
+      if (currentIndex + 1 < balls.size()) {
+        res += balls.get(currentIndex + 1);
+      }
+      if (currentIndex + 2 < balls.size()) {
+        res += balls.get(currentIndex + 2);
+      }
+      return res;
     }
+  }
+
+  static class SecondThrow implements State {
+
+    @Override
+    public int calcScore(
+      List<Integer> balls,
+      int currentScore,
+      int currentIndex
+    ) {
+      var result = 0;
+      if (isSpare(balls, currentScore, currentIndex)) {
+        result =
+          calcSecondThrowBallScoreIfSpare(balls, currentScore, currentIndex);
+      }
+
+      return result;
+    }
+
+    @Override
+    public boolean needsNextFrame() {
+      return true;
+    }
+
+    @Override
+    public State next() {
+      return new FirstThrow();
+    }
+
+    private boolean isSpare(List<Integer> balls, int score, int currentIndex) {
+      if (score + balls.get(currentIndex - 1) == 10) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    private int calcSecondThrowBallScoreIfSpare(
+      List<Integer> balls,
+      int score,
+      int currentIndex
+    ) {
+      var res = 0;
+
+      if (currentIndex + 1 < balls.size()) {
+        res += balls.get(currentIndex + 1);
+      }
+
+      return res;
+    }
+  }
+
+  public void throwBall(int score) {
+    balls.add(score);
+  }
+
+  public int score() {
+    var itsScore = 0;
+
+    var frame = new Frame();
+    State state = new FirstThrow();
+
+    for (int i = 0; i < balls.size(); i++) {
+      var currentScore = balls.get(i);
+      itsScore += currentScore;
+
+      if (frame.isLast()) {
+        continue;
+      }
+
+      itsScore += state.calcScore(balls, currentScore, i);
+      if (state.needsNextFrame()) {
+        frame.next();
+      }
+      state = state.next();
+    }
+    return itsScore;
+  }
 }
--- app/src/test/java/demo/AppTest.java
+++ app/src/test/java/demo/AppTest.java
@@ -3,13 +3,15 @@
  */
 package demo;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class AppTest {
-    @Test
-    public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+
+  @Test
+  public void appHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
 }
